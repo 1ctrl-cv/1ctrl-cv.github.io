e05d5d2f311594af857f8940eff63d0d2d4f3f13
1ctrl-cv <tzy15162169511@qq.com>
2024-04-04 21:28:12 +0800

    Site updated: 2024-04-04 21:28:10

---
+++ js/fun_title.js
@@ -0,0 +1,16 @@
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+//        $('[rel="icon"]').attr('href', "/img/trhx2.png");
+        document.title = '你干嘛去';
+        clearTimeout(titleTime);
+    }
+    else {
+//        $('[rel="icon"]').attr('href', "/img/trhx2.png");
+        document.title = '哟，回来了';
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
