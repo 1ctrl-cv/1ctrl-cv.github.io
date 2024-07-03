37b22d72ad1a84b1ac0df9ae125068694fd5f787
1ctrl-cv <tzy15162169511@qq.com>
2024-07-03 21:16:49 +0800

    Site updated: 2024-07-03 21:16:47

--- js/page_info.js
+++ js/page_info.js
@@ -1,14 +1,9 @@
-var loadTime = window.performance.timing.navigationStart - window.performance.timing.domContentLoadedEventEnd;
-window.load_time = loadTime / 10000000000000;
-window.load_time = parseFloat(window.load_time.toFixed(2));
-
-function getMemoryUsage() {
-    const mem = window.performance.memory;
-    const usedJSHeapSize = mem.usedJSHeapSize;
-    const usedJSHeapSizeMB = usedJSHeapSize / (1024 * 1024 * 8);
-    return usedJSHeapSizeMB.toFixed(3);
+function loadTime() {
+    var loadTime = window.performance.timing.fetchStart - window.performance.timing.domComplete;
+    loadTime = loadTime / 10000000000000;
+    return parseFloat(loadTime.toFixed(2));
 }
-const memory_usage = getMemoryUsage();
+const load_time = loadTime();
 
 function countRequests() {
     let count = 0;
@@ -18,6 +13,14 @@ function countRequests() {
     return count;
 }
 const request_count = countRequests();
+
+function getMemoryUsage() {
+    const mem = window.performance.memory;
+    const usedJSHeapSize = mem.usedJSHeapSize;
+    const usedJSHeapSizeMB = usedJSHeapSize / (1024 * 1024 * 8);
+    return usedJSHeapSizeMB.toFixed(3);
+}
+const memory_usage = getMemoryUsage();
 
 // console.log("耗时 " + [load_time] + " 秒 | 请求 " + [request_count] + "次 | 内存 " + [memory_usage] + " MB")
 document.write("耗时 " + [load_time] + " 秒 | 请求 " + [request_count] + " 次 | 内存 " + [memory_usage] + " MB")
