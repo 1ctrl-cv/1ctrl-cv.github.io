f1bb216bf9347d0c83684c1c21209a466d899062
1ctrl-cv <tzy15162169511@qq.com>
2024-07-15 23:18:28 +0800

    Site updated: 2024-07-15 23:18:27

--- js/fun_title.js
+++ js/fun_title.js
@@ -1,16 +1,19 @@
+// Copyright (c) B84F2246
+// Author: B84F2246
+// fun_title.js
+////////////////////////////////
 var OriginTitle = document.title;
 var titleTime;
-document.addEventListener('visibilitychange', function () {
-    if (document.hidden) {
-//        $('[rel="icon"]').attr('href', "/img/trhx2.png");
-        document.title = '你干嘛去';
-        clearTimeout(titleTime);
-    }
-    else {
-//        $('[rel="icon"]').attr('href', "/img/trhx2.png");
-        document.title = '哟，回来了';
-        titleTime = setTimeout(function () {
-            document.title = OriginTitle;
-        }, 2000);
-    }
+document.addEventListener("visibilitychange", function () {
+  if (document.hidden) {
+    // $('[rel="icon"]').attr('href', "/img/trhx2.png");
+    document.title = "你干嘛去";
+    clearTimeout(titleTime);
+  } else {
+    // $('[rel="icon"]').attr('href', "/img/trhx2.png");
+    document.title = "哟，回来了";
+    titleTime = setTimeout(function () {
+      document.title = OriginTitle;
+    }, 2000);
+  }
 });
