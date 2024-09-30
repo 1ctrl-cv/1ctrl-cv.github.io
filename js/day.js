39abe916e1661440876a894257fc11a0cfac452f
1ctrl-cv <tzy15162169511@qq.com>
2024-09-30 17:30:08 +0800

    Site updated: 2024-09-30 17:30:02

---
+++ js/day.js
@@ -0,0 +1,33 @@
+// Copyright (c) B84F2246
+// Author: B84F2246
+// day.js
+/////////////////////////
+document.write([
+    '<style type="text/css">',
+    '  .htmlgray-filter {',
+    '    filter: grayscale(100%);',
+    '    -webkit-filter: grayscale(100%);',
+    '    -moz-filter: grayscale(100%);',
+    '    -ms-filter: grayscale(100%);',
+    '    -o-filter: grayscale(100%);',
+    '    filter: url("data:image/svg+xml;utf8,#grayscale");',
+    '    filter: progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);',
+    '    -webkit-filter: grayscale(1);',
+    '  }',
+    '</style>'].join('')
+);
+
+
+var date = new Date();
+var nowMonth = date.getMonth() + 1;
+var nowDay = date.getDate();
+var nowDate = nowMonth + '-' + nowDay;
+
+var nationalMemorialDayArray = [
+    '12-13',
+];
+
+if (nationalMemorialDayArray.includes(nowDate) && location.pathname == '/') {
+    var root = document.documentElement;
+    root.className += ' htmlgray-filter';
+}
--- js/random_img.js
+++
@@ -1,4 +0,0 @@
-var random = Math.round( Math.random() * 4 );
-var bgimg = ("https://1ctrl-cv.github.io/img/background" + [random] + ".jpg");
-//console.log(bgimg);
-window.location = [bgimg] , "_self";
