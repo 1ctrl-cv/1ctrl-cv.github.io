434a0540d3665e2c82396c17569d6ff5dcb9febd
1ctrl-cv <3092193862@qq.com>
2024-11-09 23:56:21 +0800

    Site updated: 2024-11-09 23:56:20

--- js/noie.js
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Ariasaka
-// Author: Ariasaka
-// noie.js
-/////////////////////////
-if(!!window.ActiveXObject || "ActiveXObject" in window){
-    window.location.href="/noie/index.html";
-}
